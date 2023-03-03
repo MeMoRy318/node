c8fbf1fe0cd3c51d1928e982b63ff62b5a8591f5
User <girain3181@gmail.com>
2023-03-03 21:05:39 +0300

    finish

--- app.js
+++ app.js
@@ -1,20 +1,71 @@
-const event = require('node:events');
-const fs = require('node:fs/promises');
-const path = require('node:path');
+// const event = require('node:events');
+//
+// const eventsEmitter = new event();
+//
+// eventsEmitter.on('click',(args)=>{
+//     console.log(args)
+// });
+//
+// console.log(eventsEmitter.eventNames());
+//
+// eventsEmitter.once('clickAndDie',(args)=>{
+//     console.log(args)
+// })
+//
+// eventsEmitter.emit('click','hello okten');
+// eventsEmitter.emit('click1','hello world');
 
-const eventEmitter = new event();
+// const fs = require('node:fs');
+// const path = require('node:path')
 
-eventEmitter.on('click',(data)=>{
-    console.log(data)
-    console.log('hello world')
+// fs.mkdir(path.join(process.cwd(),'components',),{recursive:true},(err)=>{
+//     if (err) throw new Error(err.message)
+// });
+
+// fs.appendFile(path.join(process.cwd(),'components','text.txt'),'\n hello world',(err)=>{
+//     if (err) throw new Error(err.message)
+// });
+
+// fs.readFile(path.join(process.cwd(),'components','text.txt'),(err, data)=>{
+//     if (err) throw new Error(err.message)
+//     fs.appendFile(path.join(process.cwd(),'components','text.txt'),data,()=>{
+//         if (err) throw new Error(err.message)
+//     })
+// })
+
+// const reedStream = fs.createReadStream(path.join(process.cwd(),'components','text.txt'));
+// const writeStream = fs.createWriteStream(path.join(process.cwd(),'components','text2.txt'));
+//
+// // reedStream.on('data',(chunk)=>{
+// // writeStream.write(chunk)
+// // })
+//
+// const handelError = ()=>{
+//     console.log('error');
+//     reedStream.destroy()
+//     writeStream.end('error while pending file')
+// }
+//
+// reedStream.on('error',handelError).pipe(writeStream).on('error',handelError)
+
+
+// const readStream = fs.createReadStream(path.join(process.cwd(),'components','text.txt'));
+// const writeStream = fs.createWriteStream(path.join(process.cwd(),'components','text2.txt'));
+//
+// const handelError = ()=>{
+//     console.log('Error pending file');
+//     readStream.destroy();
+//     writeStream.end('error while pending file')
+// };
+//
+// readStream.on('error',handelError).pipe(writeStream).on('error',handelError)
+
+const express = require('express');
+
+const app = express();
+
+const PORT = 5200;
+
+app().listen(PORT,()=>{
+    console.log('server worked')
 })
-
-eventEmitter.once('clickAndDie',()=>{
-    console.log('one click and die')
-})
-
-eventEmitter.emit('click',{name:'anton'});
-eventEmitter.emit('clickAndDie');
-
-
-// fs.writeFile(path.join(process.cwd()))
